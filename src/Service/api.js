06ee5bffde7b46f0845876b3ef0f01298bfc11e5
sagar <rautsagar622@gmail.com>
2022-01-08 09:40:31 +0530

    Update api.js

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'http://localhost:8080/users';
+const usersUrl = 'https://crud-app13.herokuapp.com/users';
 
 export const getUsers = async (id) => {
     id = id || '';
